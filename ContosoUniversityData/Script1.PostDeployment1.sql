d7575fe417e68bd10a28d0cf8fdb44d70d171c70
oceanlin911 <oceanlin911@gmail.com>
2018-04-02 13:17:45 +0800

    Update DataSource

--- ContosoUniversityData/Script1.PostDeployment1.sql
+++ ContosoUniversityData/Script1.PostDeployment1.sql
@@ -14,7 +14,7 @@ MERGE INTO Student AS Target
 USING (VALUES 
         (1, 'Tibbetts', 'Donnie', '2013-09-01'), 
         (2, 'Guzman', 'Liza', '2012-01-13'), 
-(3, 'Catlett', 'Phil', '2011-09-03')
+		(3, 'Catlett', 'Phil', '2011-09-03')
 )
 AS Source (StudentID, LastName, FirstName, EnrollmentDate)
 ON Target.StudentID = Source.StudentID
---
+++ ContosoUniversityData/bin/Debug/ContosoUniversityData_1.publish.sql
@@ -0,0 +1,125 @@
+﻿/*
+ContosoSiteExample_db 的部署指令碼
+
+這段程式碼由工具產生。
+變更這個檔案可能導致不正確的行為，而且如果重新產生程式碼，
+變更將會遺失。
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "ContosoSiteExample_db"
+:setvar DefaultFilePrefix "ContosoSiteExample_db"
+:setvar DefaultDataPath ""
+:setvar DefaultLogPath ""
+
+GO
+:on error exit
+GO
+/*
+偵測 SQLCMD 模式，如果不支援 SQLCMD 模式，則停用指令碼執行。
+若要在啟用 SQLCMD 模式後重新啟用指令碼，請執行以下:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'必須啟用 SQLCMD 模式才能成功執行此指令碼。';
+        SET NOEXEC ON;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [sys].[databases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ANSI_NULLS ON,
+                ANSI_PADDING ON,
+                ANSI_WARNINGS ON,
+                ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                QUOTED_IDENTIFIER ON,
+                ANSI_NULL_DEFAULT ON 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [sys].[databases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ALLOW_SNAPSHOT_ISOLATION OFF;
+    END
+
+
+GO
+PRINT N'正在建立 [dbo].[Course]...';
+
+
+GO
+CREATE TABLE [dbo].[Course] (
+    [CourseID] INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (50) NULL,
+    [Credits]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+);
+
+
+GO
+PRINT N'正在建立 [dbo].[Enrollment]...';
+
+
+GO
+CREATE TABLE [dbo].[Enrollment] (
+    [EnrollmentID] INT            IDENTITY (1, 1) NOT NULL,
+    [Grade]        DECIMAL (3, 2) NULL,
+    [CourseID]     INT            NOT NULL,
+    [StudentID]    INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([EnrollmentID] ASC)
+);
+
+
+GO
+PRINT N'正在建立 [dbo].[Student]...';
+
+
+GO
+CREATE TABLE [dbo].[Student] (
+    [StudentID]      INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]       NVARCHAR (50) NULL,
+    [FirstName]      NVARCHAR (50) NULL,
+    [MiddleName]     NVARCHAR (50) NULL,
+    [EnrollmentDate] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC)
+);
+
+
+GO
+PRINT N'正在建立 [dbo].[FK_dbo.Enrollment_dbo.Course_CourseID]...';
+
+
+GO
+ALTER TABLE [dbo].[Enrollment] WITH NOCHECK
+    ADD CONSTRAINT [FK_dbo.Enrollment_dbo.Course_CourseID] FOREIGN KEY ([CourseID]) REFERENCES [dbo].[Course] ([CourseID]) ON DELETE CASCADE;
+
+
+GO
+PRINT N'正在建立 [dbo].[FK_dbo.Enrollment_dbo.Student_StudentID]...';
+
+
+GO
+ALTER TABLE [dbo].[Enrollment] WITH NOCHECK
+    ADD CONSTRAINT [FK_dbo.Enrollment_dbo.Student_StudentID] FOREIGN KEY ([StudentID]) REFERENCES [dbo].[Student] ([StudentID]) ON DELETE CASCADE;
+
+
+GO
